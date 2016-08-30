8e6dc04173a0337268cb642d9ae0acb289db5083
jim <eltonchan@live.com>
2016-08-30 10:17:14 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
 'use strict';
 
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
-import routerMap from './routers'
-import FastClick from 'fastclick'
-
-require('./assets/scss/weui.css');
-require('./assets/scss/common.css');
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import VueTouch from 'vue-touch';
+import routerMap from './routers';
+import FastClick from 'fastclick';
+require('./assets/scss/reset.css');
+require('./assets/scss/style.css');
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
+Vue.use(VueTouch);
 //实例化VueRouter
 let router = new VueRouter({
     hashbang: false,
     history: false
 });
+
+Vue.component('nv-head', require('./components/head.vue')); // 注册全局头部
 
 let app = Vue.extend({});
 
